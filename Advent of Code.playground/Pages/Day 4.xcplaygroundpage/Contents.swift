9fbf517e2376c220b84e7126bb86b6453e336cc3
gsbernstein <gsbernst@gmail.com>
2019-12-08 13:18:16 -0800

    working day 4

--- Advent of Code.playground/Pages/Day 4.xcplaygroundpage/Contents.swift
+++ Advent of Code.playground/Pages/Day 4.xcplaygroundpage/Contents.swift
@@ -1,14 +1,16 @@
 var count: Int = 0
 for num in 153517...630395 {
-    let hasdouble = false
     let numString = String(num)
     guard String(numString.sorted()) == numString else { continue }
-    let charCount = numString.reduce(into: [:], { result, element -> [Character:Int] in
-        result[element] = result[element] ?? 0 + 1
-    }
-    guard charCount.contains(where: { _, count -> Bool in
+    var charCount: [Character: Int] = [:]
+    
+    charCount = numString.reduce(into: charCount, { (result, element) in
+        result[element] = (result[element] ?? 0) + 1
+    })
+    let hasExactDouble: Bool = charCount.contains(where: { (_, count) -> Bool in
         count == 2
-    }) else { continue }
+    })
+    guard hasExactDouble else { continue }
 
     // guard Set(numString).count < 6 else {continue }
     print("Hit! \(num)")
