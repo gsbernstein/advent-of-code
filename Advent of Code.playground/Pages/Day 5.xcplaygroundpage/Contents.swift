2ff0ffd19112cfbe7318ac07beaed6f406844b43
gsbernstein <gsbernst@gmail.com>
2019-12-09 01:17:03 -0800

    wtf

--- Advent of Code.playground/Pages/Day 5.xcplaygroundpage/Contents.swift
+++ Advent of Code.playground/Pages/Day 5.xcplaygroundpage/Contents.swift
@@ -1,25 +1,92 @@
-var program: [Int] = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,1,6,19,23,1,10,23,27,2,27,13,31,1,31,6,35,2,6,35,39,1,39,5,43,1,6,43,47,2,6,47,51,1,51,5,55,2,55,9,59,1,6,59,63,1,9,63,67,1,67,10,71,2,9,71,75,1,6,75,79,1,5,79,83,2,83,10,87,1,87,5,91,1,91,9,95,1,6,95,99,2,99,10,103,1,103,5,107,2,107,6,111,1,111,5,115,1,9,115,119,2,119,10,123,1,6,123,127,2,13,127,131,1,131,6,135,1,135,10,139,1,13,139,143,1,143,13,147,1,5,147,151,1,151,2,155,1,155,5,0,99,2,0,14,0]
-program[1] = 41
-program[2] = 12
-// var printProgram: [String] = program.map{String($0)}
-var position = 0
-while true {
-    // print(position, program)
-    var maybeoperation: ((Int, Int) -> Int)? = nil
-    switch program[position] {
-    case 1: maybeoperation = {$0+$1}
-    case 2: maybeoperation = {$0*$1}
-    case 3:
-    case 4:
-    case 99: print("halt"); break
-    default: print("error"); break
+var program: [Int] = [
+    3,225,1,225,6,6,1100,1,238,225,104,0,1102,91,92,225,1102,85,13,225,1,47,17,224,101,-176,224,224,4,224,1002,223,8,223,1001,224,7,224,1,223,224,223,1102,79,43,225,1102,91,79,225,1101,94,61,225,1002,99,42,224,1001,224,-1890,224,4,224,1002,223,8,223,1001,224,6,224,1,224,223,223,102,77,52,224,1001,224,-4697,224,4,224,102,8,223,223,1001,224,7,224,1,224,223,223,1101,45,47,225,1001,43,93,224,1001,224,-172,224,4,224,102,8,223,223,1001,224,1,224,1,224,223,223,1102,53,88,225,1101,64,75,225,2,14,129,224,101,-5888,224,224,4,224,102,8,223,223,101,6,224,224,1,223,224,223,101,60,126,224,101,-148,224,224,4,224,1002,223,8,223,1001,224,2,224,1,224,223,223,1102,82,56,224,1001,224,-4592,224,4,224,1002,223,8,223,101,4,224,224,1,224,223,223,1101,22,82,224,1001,224,-104,224,4,224,1002,223,8,223,101,4,224,224,1,223,224,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,8,226,677,224,102,2,223,223,1005,224,329,1001,223,1,223,1007,226,226,224,1002,223,2,223,1006,224,344,101,1,223,223,108,226,226,224,1002,223,2,223,1006,224,359,1001,223,1,223,107,226,677,224,102,2,223,223,1006,224,374,101,1,223,223,8,677,677,224,102,2,223,223,1006,224,389,1001,223,1,223,1008,226,677,224,1002,223,2,223,1006,224,404,101,1,223,223,7,677,677,224,1002,223,2,223,1005,224,419,101,1,223,223,1108,226,677,224,1002,223,2,223,1005,224,434,101,1,223,223,1108,226,226,224,102,2,223,223,1005,224,449,1001,223,1,223,107,226,226,224,102,2,223,223,1005,224,464,101,1,223,223,1007,677,677,224,102,2,223,223,1006,224,479,101,1,223,223,1007,226,677,224,102,2,223,223,1005,224,494,1001,223,1,223,1008,226,226,224,1002,223,2,223,1005,224,509,1001,223,1,223,1108,677,226,224,1002,223,2,223,1006,224,524,1001,223,1,223,108,677,677,224,1002,223,2,223,1005,224,539,101,1,223,223,108,226,677,224,1002,223,2,223,1005,224,554,101,1,223,223,1008,677,677,224,1002,223,2,223,1006,224,569,1001,223,1,223,1107,677,677,224,102,2,223,223,1005,224,584,1001,223,1,223,7,677,226,224,102,2,223,223,1005,224,599,1001,223,1,223,8,677,226,224,1002,223,2,223,1005,224,614,1001,223,1,223,7,226,677,224,1002,223,2,223,1006,224,629,101,1,223,223,1107,677,226,224,1002,223,2,223,1005,224,644,1001,223,1,223,1107,226,677,224,102,2,223,223,1006,224,659,1001,223,1,223,107,677,677,224,1002,223,2,223,1005,224,674,101,1,223,223,4,223,99,226]
+
+public extension String {
+    func leftPad(to width: Int, with paddingString: String = " ") -> String {
+        if count >= width { return self }
+        let remainingLength: Int = width - count
+        let padString: [String] = (0..<remainingLength).map{ _ in paddingString }
+        return (padString + [self]).joined()
     }
-    guard let operation = maybeoperation else { break }
-    let input1position = program[position+1]
-    let input2position = program[position+2]
-    let output = operation(program[input1position],program[input2position])
-    let destination = program[position+3]
-    program[destination] = output
-    position += 4
 }
-print(program[0])
+
+public extension String {
+    
+    subscript (i: Int) -> Character {
+      return self[index(startIndex, offsetBy: i)]
+    }
+    
+    subscript (bounds: CountableClosedRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return String(self[start...end])
+    }
+
+    subscript (bounds: CountableRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return String(self[start..<end])
+    }
+}
+
+func add(x: Int, y: Int, destination: Int) {
+    let output = program[x]+program[y]
+    program[destination] = output
+}
+
+func mul(x: Int, y: Int, destination: Int) {
+    let output = program[x]*program[y]
+    program[destination] = output
+}
+
+func store(_ destination: Int, _: Int, _: Int) {
+    print("taking input")
+    program[destination] = 1
+}
+
+func output(_ value: Int, _: Int, _: Int) {
+    print("out: \(value)")
+}
+
+var position = 0
+loop: while true {
+    let instruction = String(program[position]).leftPad(to: 5, with: "0")
+    let opcode = Int(String(instruction[3...4]))!
+    let param1mode = Int(String(instruction[2]))!
+    let param2mode = Int(String(instruction[1]))!
+    var param3mode = Int(String(instruction[0]))!
+    
+    var maybeoperation: ((Int, Int, Int)->())? = nil
+    var length: Int = 1
+    
+    switch opcode {
+    case 1: maybeoperation = add(x:y:destination:); length = 4//; param3mode = 0
+    case 2: maybeoperation = mul(x:y:destination:); length = 4//; param3mode = 0
+    case 3: maybeoperation = store; length = 2
+    case 4: maybeoperation = output; length = 2
+    case 99: print("halt"); break loop
+    default: print("error, opcode was \(instruction)"); break loop
+    }
+    
+    print(String(position).leftPad(to: 4) + ": " + (position..<position+length)
+        .map{String(program[$0]).leftPad(to: 4)}
+        .joined(separator:","))
+    
+    if let operation = maybeoperation {
+        var input1 = position+1
+        if param1mode == 0 {
+            input1 = program[input1]
+        }
+        var input2 = position+2
+        if param2mode == 0 {
+            input2 = program[input2]
+        }
+        var input3 = position+3
+        if param3mode == 0 {
+            input3 = program[input3]
+        }
+        operation(input1,input2,input3)
+    }
+    position += length
+}
+//print(program[0])
