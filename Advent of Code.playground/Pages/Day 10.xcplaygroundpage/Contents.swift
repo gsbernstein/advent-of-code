5e899c8750eeb06d5e5a001e252da118f5325819
gsbernstein <gsbernst@gmail.com>
2019-12-11 12:20:17 -0800

    10.2

--- Advent of Code.playground/Pages/Day 10.xcplaygroundpage/Contents.swift
+++ Advent of Code.playground/Pages/Day 10.xcplaygroundpage/Contents.swift
@@ -112,14 +112,18 @@ let realInput = """
 ..#..#....###..........##..#...#...#..#..
 """
 
-let input = realInput
-
 struct Point: Hashable { let x, y: Int }
 
 extension Point {
     public static func + (lhs: Point, rhs: Point) -> Point {
         let x = lhs.x + rhs.x
         let y = lhs.y + rhs.y
+        return Point(x: x, y: y)
+    }
+    
+    public static func - (lhs: Point, rhs: Point) -> Point {
+        let x = lhs.x - rhs.x
+        let y = lhs.y - rhs.y
         return Point(x: x, y: y)
     }
     
@@ -139,38 +143,95 @@ extension Point: CustomStringConvertible {
         return "(\(x), \(y))"
     }
 }
-
-let array = input.split{ $0.isNewline }
-var scores = [Point:Int]()
-for (rowNo, row) in array.enumerated() {
-    for (colNo, point) in row.enumerated() {
-        if point == "#" {
-            var canSee = 0
-            var bestPerSlope = [Double:(Point,Double)]()
-            for (point,_) in scores {
-                let difX = Double(colNo - point.x)
-                let difY = Double(rowNo - point.y)
-                let dist = (difX*difX + difY*difY).squareRoot()
-                let slope = difY/difX
-                if let (_, dist2) = bestPerSlope[slope],
-                    dist2 < dist {
+func findStation(input: String) -> (Point,Int) {
+    let array = input.split{ $0.isNewline }
+    var scores = [Point:Int]()
+    for (rowNo, row) in array.enumerated() {
+        for (colNo, point) in row.enumerated() {
+            if point == "#" {
+                var canSee = 0
+                var bestPerSlope = [Double:(Point,Double)]()
+                for (point,_) in scores {
+                    let difX = Double(colNo - point.x)
+                    let difY = Double(rowNo - point.y)
+                    let dist = (difX*difX + difY*difY).squareRoot()
+                    let slope = difY/difX
+                    if let (_, dist2) = bestPerSlope[slope],
+                        dist2 < dist {
                         continue
+                    }
+                    bestPerSlope[slope] = (point,dist)
                 }
-                bestPerSlope[slope] = (point,dist)
+                for (_,(point,_)) in bestPerSlope {
+                    scores[point]! += 1
+                    canSee += 1
+                }
+                let point = Point(x: colNo, y: rowNo)
+                scores[point] = /*(scores[point] ?? 0) +*/ canSee
             }
-            for (_,(point,_)) in bestPerSlope {
-                scores[point]! += 1
-                canSee += 1
+        }
+    }
+    return scores.max(by: { (lhs, rhs) -> Bool in
+        let (_, value1) = lhs
+        let (_, value2) = rhs
+        return value1 < value2
+    })!
+}
+
+import CoreGraphics
+
+func angleBetween(_ p1: Point, _ p2: Point) -> Double {
+    let bearingPoint = p2-p1
+    let radians = atan2(Double(bearingPoint.y),Double(bearingPoint.x))
+    var degrees = radians * 180 / Double.pi
+    degrees = degrees + 90
+    return degrees >= 0 ? degrees : 360 + degrees
+}
+
+func nthToVaporize(input: String, station: Point, n: Int) -> Point {
+    let array = input.split{ $0.isNewline }
+    var pointsBySlope = [Double:[Double:Point]]()
+    for (rowNo, row) in array.enumerated() {
+        for (colNo, point) in row.enumerated() {
+            if point == "#" {
+                let point = Point(x: colNo, y: rowNo)
+                let difX = Double(station.x - point.x)
+                let difY = Double(station.y - point.y)
+                let dist = (difX*difX + difY*difY).squareRoot()
+//                let slope = difY/difX
+                let slope = angleBetween(station,point)
+                if pointsBySlope[slope] != nil {
+                    pointsBySlope[slope]![dist] = point
+                } else {
+                    pointsBySlope[slope] = [dist:point]
+                }
             }
-            let point = Point(x: colNo, y: rowNo)
-            scores[point] = /*(scores[point] ?? 0) +*/ canSee
+        }
+    }
+    let slopes = pointsBySlope.keys.sorted()
+    var count = 0
+    mainLoop: while true {
+        for slope in slopes {
+//            if count == 0 {
+//                guard slope >= 0 else { continue }
+//            }
+//            print(slope)
+//            print(pointsBySlope[slope])
+            guard let target = pointsBySlope[slope]?
+                .min(by: { $0.key < $1.key }) else { continue }
+            count += 1
+            print(count,target.value)
+            if count >= 200 { return target.value }
+            pointsBySlope[slope]!.removeValue(forKey: target.key)
         }
     }
 }
-//print(scores)
-let max = scores.max(by: { (lhs, rhs) -> Bool in
-    let (_, value1) = lhs
-    let (_, value2) = rhs
-    return value1 < value2
-})!
-print(max)
+let input = realInput
+let station = findStation(input: input).0
+//let station = Point(x: 11, y: 13)
+
+//print(angleBetween(Point(x: 0, y: 0), Point(x: 0, y: -1)))
+//print(angleBetween(Point(x: 0, y: 0), Point(x: 1, y: -1)))
+//print(angleBetween(Point(x: 0, y: 0), Point(x: 1, y: 0)))
+
+print(nthToVaporize(input: input, station: station, n: 200))
