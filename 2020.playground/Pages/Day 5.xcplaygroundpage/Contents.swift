003f4af6142e1ace587ed4307b462e88a57e57ad
Greg <greg@vida.com>
2021-01-21 15:14:32 -0800

    day 5 pt 1

---
+++ 2020.playground/Pages/Day 5.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func codeToID(_ input: String) -> Int {
+    let inputs = Array(input)
+    let rowStrings = inputs[0...6]
+    let row = stringToBinary(String(rowStrings))
+    let colStrings = inputs[7...9]
+    let col = stringToBinary(String(colStrings))
+    
+    return row * 8 + col
+}
+
+func stringToBinary(_ input: String) -> Int {
+    let inputBin: [Character] = input.map { char in
+        switch char {
+        case "B","R": return "1"
+        default: return "0"
+        }
+    }
+    return Int(String(inputBin), radix: 2)!
+}
+
+print(input1.map(codeToID(_:)).max())
+
+//: [Next](@next)
