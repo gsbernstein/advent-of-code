0b7e5e2fc12c36803e982e5d751b657590a82b15
Greg <greg@vida.com>
2021-01-21 15:51:11 -0800

    break out rules

--- 2020.playground/Pages/Day 4.xcplaygroundpage/Sources/Input.swift
+++ 2020.playground/Pages/Day 4.xcplaygroundpage/Sources/Input.swift
@@ -1,44 +1,5 @@
 import Foundation
 
-public let reqd: [String: (String) -> Bool] = [
-    "byr": { Int($0).map { (1920...2002).contains($0) } ?? false }, // (Birth Year)
-    "iyr": { Int($0).map { (2010...2020).contains($0) } ?? false }, // (Issue Year)
-    "eyr": { Int($0).map { (2020...2030).contains($0) } ?? false }, // (Expiration Year)
-    "hgt": validHeight, // (Height)
-    "hcl": validHex, // (Hair Color)
-    "ecl": ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains, // (Eye Color)
-    "pid": validPID // (Passport ID)
-//    "cid" // (Country ID)
-]
-
-func validHeight(_ string: String) -> Bool {
-    var string = string
-    if string.hasSuffix("in") {
-        string.removeLast(2)
-        guard let int = Int(string) else { return false }
-        return (59...76).contains(int)
-    } else if string.hasSuffix("cm") {
-        string.removeLast(2)
-        guard let int = Int(string) else { return false }
-        return (150...193).contains(int)
-    } else {
-        return false
-    }
-}
-
-let hexSet = CharacterSet(charactersIn: "01234567890ABCDEFabcdef")
-
-func validHex(_ string: String) -> Bool {
-    guard string.hasPrefix("#") else { return false }
-    var string = string
-    string.removeFirst()
-    return string.rangeOfCharacter(from: hexSet.inverted) == nil
-}
-
-func validPID(_ string: String) -> Bool {
-    return string.count == 9 && Int(string) != nil
-}
-
 public let testInput = """
 ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
 byr:1937 iyr:2017 cid:147 hgt:183cm
