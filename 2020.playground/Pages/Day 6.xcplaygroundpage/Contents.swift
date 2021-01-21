901dfd284701dc3b26048ebc8776be89f7e37a99
Greg <greg@vida.com>
2021-01-21 15:42:15 -0800

    day 6 pt 2
    intersection

--- 2020.playground/Pages/Day 6.xcplaygroundpage/Contents.swift
+++ 2020.playground/Pages/Day 6.xcplaygroundpage/Contents.swift
@@ -5,17 +5,17 @@ import Foundation
 func parse(input: String) -> Int {
     return input
         .components(separatedBy: "\n\n")
-        .map { Set($0.filter { $0 != "\n"}) }
+        .map(answers)
         .map { $0.count }
         .reduce(0, +)
 }
 
-func answers(_ input: String) -> String {
-    input.reduce(into: "") { (result, char) in
-        if !result.contains(char) {
-            result.append(char)
+func answers(_ input: String) -> Set<Character> {
+    input.components(separatedBy: "\n")
+        .reduce(into: Set("abcdefghijklmnopqrstuvwxyz")) { (result, string) in
+            let set = Set(string)
+            result.formIntersection(set)
         }
-    }
 }
 
 print(parse(input: input1))
