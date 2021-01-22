c53cf38ec886bd428f15590ab26ee41b416af9bd
Greg <greg@vida.com>
2021-01-21 17:04:21 -0800

    Day 9 pt 2

--- 2020.playground/Pages/Day 9.xcplaygroundpage/Contents.swift
+++ 2020.playground/Pages/Day 9.xcplaygroundpage/Contents.swift
@@ -27,7 +27,30 @@ func isValid(_ value: Int, _ memory: [Int]) -> Bool {
 }
 
 //check(testInput, preamble: 5)
-check(input1, preamble: 25)
+//check(input1, preamble: 25)
 
+let value = 22406676
+
+let testVal = 127
+
+func contig(value: Int, mem: [Int]) {
+    outer: for (index, mem1) in mem.enumerated() {
+        var sum = mem1
+        var array = Set([mem1])
+        for mem2 in mem[(index+1)...] {
+            sum += mem2
+            array.insert(mem2)
+            if sum == value {
+                let min = array.min()!
+                let max = array.max()!
+                print(min+max)
+                break outer
+            }
+            if sum > value { continue outer }
+        }
+    }
+}
+
+contig(value: value, mem: input1)
 
 //: [Next](@next)
